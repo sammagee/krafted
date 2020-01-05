d388cfb4f405fa07b4c6ebf1ad0d6e79094179ea
Sam <s@sammagee.me>
2020-01-05 17:01:09 -0600

    Fix hover overlay on projects on Work page

--- resources/js/Pages/Work/Index.vue
+++ resources/js/Pages/Work/Index.vue
@@ -9,7 +9,7 @@
                 <a :href="project.slug" class="group flex flex-col relative hover:z-50 transition-all ease-in-out transition-slow">
                     <div class="relative overflow-hidden rounded-lg shadow-md hover:shadow-xl transition-all ease-in-out transition-slow z-10">
                         <img class="rounded-lg filter-grayscale filter-primary group-hover:filter-none transition-all ease-in-out transition-slow" :src="'/storage/' + project.image_url" :alt="project.title" />
-                        <div class="absolute inset-0 bg-primary-500 opacity-50 hover:opacity-25 transition-opacity ease-in-out transition-slow"></div>
+                        <div class="absolute inset-0 bg-primary-500 opacity-50 group-hover:opacity-25 transition-opacity ease-in-out transition-slow"></div>
                     </div>
 
                     <div class="flex justify-between absolute top-full inset-x-0 pt-6 px-3 pb-2 -mt-4 mx-2 bg-primary-800 rounded-b-lg shadow-lg opacity-0 group-hover:opacity-100 group-hover:h-auto transition-all ease-in-out transition-slow">
